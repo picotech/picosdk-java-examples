1a0d2f69bd01ad6046837539e2c8d58a8149c778
hitesh.mistry <hsmistry@users.noreply.github.com>
2017-07-20 11:35:08 +0100

    Add ps4000 library example

---
+++ ps4000/PicoScope4000JNA/src/com/picotech/picoscope/ps4000/jna/PS4000CLibrary.java
@@ -0,0 +1,105 @@
+/**************************************************************************
+ *
+ * Filename:    PS4000CLibrary.java
+ *
+ * Author:      HSM
+ *
+ * Description: 
+ *  This interface defines functions and enumerations from the ps4000Api.h 
+ * C header file for the PicoScope 4000 Series oscilloscopes using the 
+ * ps4000 driver API functions.
+ * 
+ * Copyright © 2014-2017 Pico Technology Ltd. See LICENSE file for terms.
+ * 
+ ***************************************************************************/
+
+package com.picotech.picoscope.ps4000.jna;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.ptr.ShortByReference;
+
+public interface PS4000CLibrary extends Library
+{
+	
+	PS4000CLibrary INSTANCE = (PS4000CLibrary) Native.loadLibrary
+                                    (
+                                        "ps4000", PS4000CLibrary.class
+                                    );
+	
+        // Method declarations
+        // ===================
+        
+        // C prototype definition : 
+	//      uint32_t ps4000EnumerateUnits(int16_t * count, int8_t * serials, int16_t serialLth)
+        int ps4000EnumerateUnits(ShortByReference count, byte[] serials, ShortByReference serialLth);
+        
+	// C prototype definition :
+        //      uint32_t ps4000OpenUnit(int16_t * handle)             
+        int ps4000OpenUnit(ShortByReference handle);
+	
+	// C prototype definition
+	//      uint32_t ps4000CloseUnit(int16_t handle);
+	int ps4000CloseUnit(short handle);
+	
+        // C prototype definition
+        //      unit32_t ps4000GetUnitInfo(int16_t handle, int8_t *string, int16_t stringLength,
+        //                                  int16_t *requiredSize, PICO_INFO info)
+        int ps4000GetUnitInfo(short handle, byte[] string, short stringLength, ShortByReference requiredSize, int info);
+        
+        
+        // Enumerations
+        // ============
+
+        public enum PS4000Channel
+        {
+            PS4000_CHANNEL_A(0),
+            PS4000_CHANNEL_B(1),
+            PS4000_CHANNEL_C(2),
+            PS4000_CHANNEL_D(3),
+            PS4000_EXTERNAL(4),
+            PS4000_MAX_CHANNELS(4),
+            PS4000_TRIGGER_AUX(5),
+            PS4000_MAX_TRIGGER_SOURCES(6);
+            
+            private final int channel;
+
+            PS4000Channel(int channel)
+            {
+                this.channel = channel;
+            }
+        }
+
+        public enum PS4000Range
+        {
+            PS4000_10MV,
+            PS4000_20MV,
+            PS4000_50MV,
+            PS4000_100MV,
+            PS4000_200MV,
+            PS4000_500MV,
+            PS4000_1V,
+            PS4000_2V,
+            PS4000_5V,
+            PS4000_10V,
+            PS4000_20V,
+            PS4000_50V,
+            PS4000_100V,
+            PS4000_MAX_RANGES
+        }
+
+        public enum PS4000RatioMode
+        {
+            RATIO_MODE_NONE(0),
+            RATIO_MODE_AGGREGATE(1),
+            RATIO_MODE_AVERAGE(2);
+
+            private final int ratio;
+
+            PS4000RatioMode(int ratio)
+            {
+                this.ratio = ratio;
+            }
+        }
+}
+   
---
+++ ps4000/PicoScope4000JNA/src/com/picotech/picoscope/ps4000/jna/PicoScope4000JNA.java
@@ -0,0 +1,156 @@
+/**************************************************************************
+ *
+ * Filename:    PicoScope4000JNA.java
+ *
+ * Author:      HSM
+ *
+ * Description: 
+ *  This is an example program demonstrating how to use Java and Java
+ * Native Access (JNA) in order to call functions from the ps4000 library
+ * for PicoScope 4000 series oscilloscopes in order to:
+ * 
+ *      Open a connection to a unit
+ *      Print unit information
+ *      Close the connection to the unit
+ * 
+ * Supported PicoScope models:
+ *
+ *      PicoScope 4224 & 4424
+ *      PicoScope 4226 & 4227 
+ *	PicoScope 4262 
+ * 
+ * Copyright © 2014-2017 Pico Technology Ltd. See LICENSE file for terms.
+ * 
+ ***************************************************************************/
+package com.picotech.picoscope.ps4000.jna;
+
+import com.sun.jna.ptr.ShortByReference;
+import com.picotech.picoscope.*;
+import com.sun.jna.Native;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+
+public class PicoScope4000JNA
+{
+    protected Scanner consoleScanner;
+    
+    public PicoScope4000JNA()
+    {
+        consoleScanner = new Scanner(System.in);
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        PicoScope4000JNA ps4000JNA = new PicoScope4000JNA();
+        
+        System.out.println("Searching for PicoScope 4000 Series devices...");
+        System.out.println();
+        
+        ShortByReference countRef = new ShortByReference();
+        byte[] serials = new byte[100];
+        ShortByReference serialsLthRef = new ShortByReference();
+        serialsLthRef.setValue((short) serials.length);
+        
+        int enumerateStatus = PS4000CLibrary.INSTANCE.ps4000EnumerateUnits(countRef, serials, serialsLthRef);
+        
+        System.out.println("Enumerate status: " + enumerateStatus);
+        System.out.println("Count: " + countRef.getValue());
+        
+        String deviceSerials = "";
+        
+        try
+        {
+            deviceSerials = new String(serials, "UTF-8");
+            
+        }
+        catch(UnsupportedEncodingException uee)
+        {
+            deviceSerials = "";
+            
+        }
+        
+        System.out.println("Serial number(s): " +  Native.toString(serials));
+        System.out.println("Serials Lth: " + serialsLthRef.getValue());
+        System.out.println();
+        
+        // Prompt user to start
+        
+        System.out.println("Press any key to begin.");
+        
+        ps4000JNA.getAnyKeyInput();
+        
+        System.out.println("Opening PicoScope 4000 Series device ...");
+        System.out.println("");
+        
+        ShortByReference handleRef = new ShortByReference();
+        handleRef.setValue((short) 0);
+        
+                
+        //String deviceSerial = "AR290/071"; // Change to your serial number
+        String deviceSerial = null; // Change to your serial number
+       
+        int openStatus = PicoStatus.PICO_OK;
+        
+        try
+        {
+            openStatus = PS4000CLibrary.INSTANCE.ps4000OpenUnit(handleRef);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        
+        if (openStatus == PicoStatus.PICO_OK)
+        {
+            System.out.println("Found PicoScope 4000 Series device:-");
+        
+            short handle = handleRef.getValue();
+
+            System.out.println("Handle: " + handle);
+
+            byte[] infoBytes = new byte[40];
+            ShortByReference reqSizeRef = new ShortByReference();
+
+            int driverInfoStatus = PS4000CLibrary.INSTANCE.ps4000GetUnitInfo(handle, infoBytes, (short) infoBytes.length, reqSizeRef, PicoInfo.PICO_DRIVER_VERSION);
+
+            System.out.println("Req Size: " + reqSizeRef.getValue());
+
+            String driverInfo = new String();
+
+            try
+            {
+                driverInfo = new String(infoBytes, "UTF-8");
+            }
+            catch(UnsupportedEncodingException uee)
+            {
+                driverInfo = "";
+
+            }
+
+            System.out.println("Driver: " + driverInfo);
+
+            reqSizeRef.setValue((short) infoBytes.length);
+            int variantInfoStatus = PS4000CLibrary.INSTANCE.ps4000GetUnitInfo(handle, infoBytes, (short) infoBytes.length, reqSizeRef, PicoInfo.PICO_VARIANT_INFO);
+
+            System.out.println("Variant: " + Native.toString(infoBytes));
+
+            PS4000CLibrary.INSTANCE.ps4000CloseUnit(handle);
+        }
+    }
+    
+    public synchronized Scanner getConsoleScanner()
+    {
+        return consoleScanner;
+    }
+    
+    public void getAnyKeyInput()
+    {
+        String key_input = getConsoleScanner().nextLine();
+        
+    }
+    
+}
